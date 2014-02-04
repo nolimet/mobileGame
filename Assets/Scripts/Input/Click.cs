4318565b0cda2cf02ab7a3af592804e6ea4caf56
Nolimet <nolimet.stamdata.nl>
2014-02-04 12:29:55 +0100

    spoofed

--- Assets/Scripts/Input/Click.cs
+++ Assets/Scripts/Input/Click.cs
@@ -2,17 +2,33 @@
 using System.Collections;
 
 public class Click : MonoBehaviour {
+    public int LoadLevel;
+    public bool ExitGame;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        if (Input.touchCount != 0)
+    void Update()
+    {
+        // Code for OnMouseDown in the iPhone. Unquote to test.
+        RaycastHit hit = new RaycastHit();
+        for (int i = 0; i < Input.touchCount; ++i)
         {
-            //Input.
+            if (Input.GetTouch(i).phase.Equals(TouchPhase.Began))
+            {
+                // Construct a ray from the current touch coordinates
+                Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(i).position);
+                if (Physics.Raycast(ray, out hit))
+                {
+                    hit.transform.gameObject.SendMessage("OnMouseDown");
+                }
+            }
         }
-	}
+    }
+    void OnMouseDown()
+    {
+        if (ExitGame&&!Application.isWebPlayer&&!Application.isEditor)
+        {
+            Application.Quit();
+        }
+        Application.LoadLevel(LoadLevel);
+        Debug.Log("clicked");
+    }
 }
---
+++ Assets/Scripts/UI/FlyInObject.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlyInObject : MonoBehaviour {
+
+    public float WaitForSec = 0f;
+    public Vector2 MoveAmount;
+    private Vector2 origen;
+	// Use this for initialization
+	void Start () {
+        origen = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (WaitForSec > 0)
+        {
+            WaitForSec -= Time.deltaTime;
+            return;
+        }
+
+        Vector2 temp1 = origen + MoveAmount;
+        Vector2 temp2 = transform.position;
+        Debug.Log(Vector2.Distance(temp2, temp1));
+        if (Vector2.Distance(temp2, temp1) > 0.1f)
+        {
+            Vector2 dir = temp1 - temp2;
+            transform.Translate(dir * Time.deltaTime);
+        }
+        
+	}
+}
