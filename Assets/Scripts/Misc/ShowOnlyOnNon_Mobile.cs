5ca22159e3ec9d2c9a79cc44a50f5a436a2f2608
Nolimet <nolimet@stamdata.nl>
2014-03-25 12:18:16 +0100

    added some text
    Game will give a warning when you try and run it in menu

---
+++ Assets/Scripts/Misc/ShowOnlyOnNon_Mobile.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+//using System.Collections.Generic;
+using UnityEngine;
+
+namespace Misc
+{
+    class ShowOnlyOnNon_Mobile : MonoBehaviour
+    {
+        void Start()
+        {
+            if (Application.platform == RuntimePlatform.Android)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+    }
+}
